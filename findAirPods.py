49e30a79a50a7c0e2dfa2914d3fc128b369a1de9
theromit <rpmehta@mymehta.net>
2016-12-26 19:12:22 -0800

    Very rough script to find AirPods availability
    Currently hard-coded to Cupertino (95014) - may need tweaking to take input and also to accept more than one location
    Also, there is a br in the results that needs to be stripped out ... later

---
+++ findAirPods.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+baseUrl = 'http://www.apple.com/shop/retail/pickup-message?parts.0=MMEF2AM%2FA&location='
+testZip = '95014'
+
+r = requests.get(baseUrl+testZip)
+if r.status_code <> 200:
+    print "error"
+
+x = json.loads(r.text)
+
+totResults = len(x['body']['stores'])
+
+for i in range(0,totResults):
+    print 'For store: ' + x['body']['stores'][i]['storeName'] + ' -- ' + x['body']['stores'][i]['partsAvailability']['MMEF2AM/A']['pickupSearchQuote']
