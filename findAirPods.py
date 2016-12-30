fa5d962a2f6e1abe5f3f2813fb06d382cca2af55
theromit <rpmehta@mymehta.net>
2016-12-29 21:42:11 -0800

    Parameterized location and product
    Instead of using a hard-coded zip and product code, made both arguments to the script. 
    
    Usage: findAirPods.py <zip> <product code>
    
    This means, this is no longer a "find AirPods" script but all Apple products :-)

--- findAirPods.py
+++ findAirPods.py
@@ -1,8 +1,18 @@
 import requests
 import json
+import sys 
+import argparse
 
-baseUrl = 'http://www.apple.com/shop/retail/pickup-message?parts.0=MMEF2AM%2FA&location='
-testZip = '95014'
+parser = argparse.ArgumentParser()
+parser.add_argument("location", help="Enter US zip code where you want to search")
+parser.add_argument("product", help="Enter Apple product code to search (easiest to get this code by adding it to cart)")
+args = parser.parse_args()
+
+prod = args.product
+prodcode = prod.replace("/", "%2F") #need to do this for the URL 
+testZip = args.location
+
+baseUrl = 'http://www.apple.com/shop/retail/pickup-message?parts.0=' + prodcode + '&location='
 
 r = requests.get(baseUrl+testZip)
 if r.status_code <> 200:
@@ -14,7 +24,6 @@ totResults = len(x['body']['stores'])
 
 for i in range(0,totResults):
     storename = x['body']['stores'][i]['storeName']
-    storeavail = x['body']['stores'][i]['partsAvailability']['MMEF2AM/A']['pickupSearchQuote']
+    storeavail = x['body']['stores'][i]['partsAvailability'][prod]['pickupSearchQuote']
     storeavail = storeavail.replace("<br/>", " ")
-    #print 'For store: ' + x['body']['stores'][i]['storeName'] + ' -- ' + x['body']['stores'][i]['partsAvailability']['MMEF2AM/A']['pickupSearchQuote']
     print 'For store: ' + storename + ' -- ' + storeavail
